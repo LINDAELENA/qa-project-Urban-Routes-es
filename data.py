bd58c96abac8aaea66d124ed97e4148e5785a56d
LINDAELENA <lc-herrera@outlook.com>
2024-06-25 17:24:13 -0600

    Se reemplaza la URL de Urban Routes para poder acceder al servidor.

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://029f5e89-1ed2-457a-8c61-c3ff8b9e57c2.serverhub.tripleten-services.com'
+urban_routes_url = 'https://cnt-52dd7de9-044a-4008-a7a8-881f8b6cb2cb.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
